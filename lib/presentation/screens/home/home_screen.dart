454e9d25a0c4f10242e52d64dd0f61ec60d99e05
XRedX <129623048+XRed8X@users.noreply.github.com>
2025-12-29 19:46:45 -0600

    Basic routes implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,10 +8,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const _HomeView()
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: const _HomeView(),
     );
   }
 }
@@ -20,22 +19,18 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return ListView.builder(
       itemCount: appMenuItems.length,
       itemBuilder: (context, index) {
         final menuItem = appMenuItems[index];
         return _CustomListTile(menuItem: menuItem);
-      }
+      },
     );
   }
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({
-    required this.menuItem,
-  });
+  const _CustomListTile({required this.menuItem});
 
   final MenuItem menuItem;
 
@@ -45,12 +40,14 @@ class _CustomListTile extends StatelessWidget {
     return ListTile(
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      leading: Icon(menuItem.icon, color: colors.primary,),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
-      onTap:() {
-        
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      onTap: () {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute<void>(builder: (context) => const ButtonsScreen()),
+        // );
+        Navigator.pushNamed(context, menuItem.link);
       },
-    
     );
   }
 }
